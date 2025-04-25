9710ac18c064ab5b0cbdb702443468bc15446ae4
xX-Higgs-Xx <133397981+xX-Higgs-Xx@users.noreply.github.com>
2025-04-24 23:10:47 -0600

    Creacion de proyecto para practicar java

---
+++ proyecto-1/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Jared Adrian Juarez Bernal";
+        System.out.println(name.toUpperCase());
+
+        int number = 11;
+        System.out.println("number = " + number);
+
+        boolean valor = true;
+        int valor2 = 10;
+        if (valor){
+            valor2 = 5;
+            System.out.println("valor2 = " + valor2);
+        }
+        System.out.println("valor2 = " + valor2);
+
+        var number3 = "15";
+        System.out.println("number3 = " + number3);
+
+        name = "Solo Adrian";
+        if(number > 10) {
+            name = "Jared Adrian Juarez Bernal";
+        }
+        System.out.println("name = " + name);
+    }
+}
