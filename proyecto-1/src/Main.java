adf2f65269c949090139385a9b02832b2f63e5b2
xX-Higgs-Xx <133397981+xX-Higgs-Xx@users.noreply.github.com>
2025-04-25 23:17:57 -0600

    comentarios para seguir con el proyecto 1

--- proyecto-1/src/Main.java
+++ proyecto-1/src/Main.java
@@ -22,5 +22,7 @@ public class Main {
             name = "Jared Adrian Juarez Bernal";
         }
         System.out.println("name = " + name);
+
+        // La meta de este proyecto es desarrollar una aplicacion completa por mi mismo
     }
 }
